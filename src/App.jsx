ca340d48349375ca351f243267137ce56e66a126
Joshtri <stuffofyos1516@gmail.com>
2024-07-17 22:58:36 +0800

    fix routing redirect

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename='/'>
         <Routes>
           <Route path="/" element={<Dashboard/>}/>
           <Route path="/about" element={<About/>}/>
