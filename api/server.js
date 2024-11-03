7f434673d94ad014d623e47c17e2b6029e37b270
Joshtri <stuffofyos1516@gmail.com>
2024-11-03 15:48:23 +0800

    updating server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import path from 'path';
-import prerender from 'prerender-node';
 import { fileURLToPath } from 'url';
 
 // Untuk mendapatkan __dirname dalam modul ES
@@ -9,16 +8,17 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Konfigurasi Prerender.io
-prerender.set('prerenderToken', 'YOUR_PRERENDER_IO_TOKEN');
-app.use(prerender);
+// Middleware Prerender.io
+// eslint-disable-next-line no-undef
+app.use(require('prerender-node').set('prerenderToken', `${process.env.VITE_PRERENDER_TOKEN}`));
 
 // Sajikan file build dari Vite
 app.use(express.static(path.join(__dirname, '../dist')));
 
+// Arahkan semua rute ke index.html untuk mendukung rute SPA
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
-// Ekspor handler untuk Vercel
+// Ekspor handler untuk digunakan oleh Vercel
 export default app;
