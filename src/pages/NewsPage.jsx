387b5effd861ab9c6db2c922a0951487c130a20b
Joshtri <stuffofyos1516@gmail.com>
2024-11-03 15:20:44 +0800

    updating news page for meta

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from 'react';
 import Layout from './Layout';
 import News from '../components/News/NewsCard';
+import { Helmet } from 'react-helmet-async';
+
 function NewsPage() {
   return (
     <>
-        <Layout>
-            <News/>
-        </Layout>
+      <Helmet>
+        <title>News - Tech Talk Blog</title>
+        <meta name="description" content="Berita terbaru dan terkini di Tech Talk Blog." />
+        <meta property="og:title" content="News - Tech Talk Blog" />
+        <meta property="og:description" content="Berita terbaru dan terkini di Tech Talk Blog." />
+        <meta property="og:image" content="https://tech-talks-blog.com/news-image.jpg" />
+        <meta property="og:url" content="https://tech-talks-blog.com/news" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="News - Tech Talk Blog" />
+        <meta name="twitter:description" content="Berita terbaru dan terkini di Tech Talk Blog." />
+        <meta name="twitter:image" content="https://tech-talks-blog.com/news-image.jpg" />
+      </Helmet>
+      <Layout>
+        <News />
+      </Layout>
     </>
-  )
+  );
 }
 
-export default NewsPage
+export default NewsPage;
