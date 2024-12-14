78871730c09cea9ce2762d303944a150b59a50f6
Joshtri <stuffofyos1516@gmail.com>
2024-12-14 11:03:54 +0800

    updating searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,3 @@
-// SearchBar.js
 import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
@@ -13,19 +12,19 @@ const SearchBar = ({ onSearch }) => {
 
   return (
     <div className="flex justify-center my-6">
-      <div className="relative flex items-center bg-white shadow-md rounded-full w-full max-w-lg transition-all duration-300">
+      <div className="relative flex items-center bg-white dark:bg-gray-800 shadow-md rounded-full w-full max-w-lg transition-all duration-300">
         {/* Ikon Pencarian */}
         <div className="flex items-center pl-4">
           <FaSearch
-            className={`text-gray-400 transition-all duration-300 ${
-              isFocused ? 'text-blue-500' : ''
+            className={`text-gray-400 dark:text-gray-300 transition-all duration-300 ${
+              isFocused ? 'text-blue-500 dark:text-blue-400' : ''
             }`}
           />
         </div>
         {/* Input */}
         <input
           type="text"
-          className="flex-grow bg-transparent border-none focus:ring-0 focus:border-none focus:outline-none px-4 py-3 text-gray-700 placeholder-gray-400"
+          className="flex-grow bg-transparent border-none focus:ring-0 focus:border-none focus:outline-none px-4 py-3 text-gray-700 placeholder-gray-400 dark:text-gray-200 dark:placeholder-gray-400"
           placeholder="Cari artikel di blog..."
           value={searchTerm}
           onChange={handleInputChange}
@@ -34,7 +33,7 @@ const SearchBar = ({ onSearch }) => {
         />
         {/* Tombol Cari */}
         <button
-          className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white px-6 py-2 rounded-full transition duration-300 shadow hover:shadow-lg focus:ring-2 focus:ring-blue-400 absolute right-0 mr-1"
+          className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 dark:from-blue-600 dark:to-blue-700 dark:hover:from-blue-700 dark:hover:to-blue-800 text-white px-6 py-2 rounded-full transition duration-300 shadow hover:shadow-lg focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-500 absolute right-0 mr-1"
           onClick={() => onSearch(searchTerm)}
         >
           Cari
