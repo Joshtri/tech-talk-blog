ac3dfc19de39023f1284c81a773a21f321e2b1fa
Joshtri <stuffofyos1516@gmail.com>
2024-12-31 00:08:52 +0800

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,23 @@
-// import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Main from './pages/MainPage';
-import PostDetail from './pages/PostDetail';
-import About from './pages/About';
-import NewsPage from './pages/NewsPage';
-import GamesPage from './pages/GamesPage';
 import { HelmetProvider } from 'react-helmet-async';
-import VoiceLabPage from './pages/VoiceLabPage';
-import LiveChatPage from "./pages/LiveChatPage";
-import CSHubPage from "./pages/CsHubPage";
-import AudioCutterPage from "./pages/AudioCutterPage";
-import FaceDetectionExpressionPage from "./pages/FaceDetectionExpressionPage";
-import TechTalkPlaylist from "./pages/TechTalkVideoPages/TechTalkPlaylist";
 import Snowfall from "./components/SnowFall";
-import AdBanner from "./components/AdBanner";
-
+import allRoutes from "./routes/allRoutes";
 
 function App() {
-  
   return (
     <>
-                              {/* Menampilkan Banner Iklan */}
-                              {/* <AdBanner /> */}
-        <Snowfall /> {/* Tambahkan di sini */}
+      <Snowfall />
       <HelmetProvider>
-
-        <BrowserRouter basename='/'>
+        <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Main/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/post/:slug" element={<PostDetail/>} />
-            <Route path='/games' element={<GamesPage/>}/>
-
-            <Route path='/news' element={<NewsPage/>}/>
-            <Route path='/voice-labs' element={<VoiceLabPage/>}/>
-
-
-            <Route path="/audio-cutter" element={<AudioCutterPage/>}/>
-            <Route path="/face-detection-expression" element={<FaceDetectionExpressionPage/>}/>
-            <Route path='/live-chat' element={<LiveChatPage/>}/>
-            <Route path="/cs-hub" element={<CSHubPage />} />
-            <Route path="/tech-talk-video" element={<TechTalkPlaylist />} />
-
-    
+            {allRoutes.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
           </Routes>
         </BrowserRouter>
       </HelmetProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
