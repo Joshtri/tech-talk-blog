5aa6e082c69b84817d4f394761af85a54f44a3af
Joshtri <stuffofyos1516@gmail.com>
2025-01-13 10:26:17 +0800

    remove loading, jelek

--- src/App.jsx
+++ src/App.jsx
@@ -28,14 +28,14 @@ function App() {
   }, []);
 
   // Tampilkan spinner loading saat memuat status maintenance
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center h-screen bg-gray-400">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-opacity-75"></div>
-        <p className="ml-4 text-lg font-semibold text-blue-600">Loading...</p>
-      </div>
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <div className="flex items-center justify-center h-screen bg-gray-400">
+  //       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-opacity-75"></div>
+  //       <p className="ml-4 text-lg font-semibold text-blue-600">Loading...</p>
+  //     </div>
+  //   );
+  // }
 
   // Jika dalam mode maintenance, tampilkan halaman maintenance
   if (isMaintenance) {
