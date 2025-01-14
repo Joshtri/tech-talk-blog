4d616e8ad2b71c4ff60c88ee318b1597bbb10548
Joshtri <stuffofyos1516@gmail.com>
2025-01-14 12:11:06 +0800

    Refactor import paths for components to follow proper casing conventions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Menentukan output directory untuk build
+    distDir: "dist",
+  
+    // Optimasi tambahan
+    reactStrictMode: true, // Aktifkan mode ketat untuk React
+    swcMinify: true, // Aktifkan SWC untuk mempercepat proses build
+    images: {
+      domains: ["https://tech-talks-blog.com"], // Tambahkan domain untuk mengizinkan pemrosesan gambar eksternal
+    },
+    experimental: {
+      appDir: true, // Aktifkan App Directory jika menggunakan layout routing baru
+    },
+  };
+  
+  export default nextConfig;
+  
